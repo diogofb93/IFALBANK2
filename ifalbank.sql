b99dea747c0acc4e16c656ce2c93795862bc7c38
Diogo <Diogo@Diogo-PC>
2016-01-04 18:04:43 -0200

    4.5.1

--- ifalbank.sql
+++ ifalbank.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 03-Jan-2016 às 23:29
+-- Generation Time: 04-Jan-2016 às 21:03
 -- Versão do servidor: 10.1.9-MariaDB
 -- PHP Version: 5.6.15
 
@@ -41,7 +41,20 @@ CREATE TABLE `cliente` (
 
 INSERT INTO `cliente` (`ID_USUARIO`, `NOME`, `SENHA`, `TIPO`, `SALDO`, `CPF`) VALUES
 (6, 'Diogo', '1234', 'gerente', 155555.29, '11171147406'),
-(7, 'teste', '1234', 'cliente', 222.55, '43421');
+(7, 'teste', '1234', 'poupanca', 25.00, '43421'),
+(8, 'teste', '1234', 'corrente', 68.00, '1111212121');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `transacao`
+--
+
+CREATE TABLE `transacao` (
+  `ID_TRANSACAO` int(20) NOT NULL,
+  `ID_USUARIO` int(11) NOT NULL,
+  `TIPO` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -55,6 +68,12 @@ ALTER TABLE `cliente`
   ADD UNIQUE KEY `CPF` (`CPF`);
 
 --
+-- Indexes for table `transacao`
+--
+ALTER TABLE `transacao`
+  ADD PRIMARY KEY (`ID_TRANSACAO`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -62,7 +81,12 @@ ALTER TABLE `cliente`
 -- AUTO_INCREMENT for table `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `ID_USUARIO` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID_USUARIO` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `transacao`
+--
+ALTER TABLE `transacao`
+  MODIFY `ID_TRANSACAO` int(20) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
